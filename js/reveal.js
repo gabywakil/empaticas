51c5f69df8e47367febd1d61ffc0ac3986b7228f
gabywakil <149974228+gabywakil@users.noreply.github.com>
2026-03-30 17:13:58 -0500

    Update reveal.js

--- js/reveal.js
+++ js/reveal.js
@@ -1,10 +1,10 @@
 // reveal.js
 const o=new IntersectionObserver(es=>{
-    es.forEach((e,i)=>{
-      if(e.isIntersecting){
-        setTimeout(()=>e.target.classList.add('on'),i*75);
-        o.unobserve(e.target);
-      }
-    });
-  },{threshold:.08,rootMargin:'0px 0px -16px 0px'});
-  document.querySelectorAll('.rv').forEach(el=>o.observe(el));
+  es.forEach((e,i)=>{
+    if(e.isIntersecting){
+      setTimeout(()=>e.target.classList.add('on'),i*75);
+      o.unobserve(e.target);
+    }
+  });
+},{threshold:.08,rootMargin:'0px 0px -16px 0px'});
+document.querySelectorAll('.rv').forEach(el=>o.observe(el));
